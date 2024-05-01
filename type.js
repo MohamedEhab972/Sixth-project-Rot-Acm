092e8a431c6863fe7dbd5c56c4b072eef72d9a27
Mohamed Ehab <Mohamedehab972@gmail.com>
2024-05-01 22:19:14 +0300

    the last task with bootestrap

---
+++ type.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#typed", {
+  strings: ["Freelancer", "Developar", "Designer", "Photographer"],
+  typeSpeed: 80,
+  startDelay: 500,
+  backDelay: 1000,
+  backSpeed: 80,
+  // fadeOut: true,
+  loop: true,
+  loopCount: 2,
+  // showCursor: false
+  // cursorChar: '...'
+});
